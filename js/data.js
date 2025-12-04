781044c21cbe046add36af8060e14fcfe5c95eb2
Sam <smauer@users.noreply.github.com>
2025-12-04 09:25:44 -0600

    Change to fetch affirmation

--- js/data.js
+++ js/data.js
@@ -24,21 +24,36 @@ var SLIDES = {
 }
 var slideIdx = -1;
 const nextSlide = function() {
-    if (++slideIdx > SLIDES.data.length-1) {
-        slideIdx = 0;
-    }
-    console.log(SLIDES.data[slideIdx]);
-    setSlide(SLIDES.data[slideIdx]);
+    //if (++slideIdx > SLIDES.data.length-1) {
+    //    slideIdx = 0;
+    //}
+    //console.log(SLIDES.data[slideIdx]);
+    //setSlide(SLIDES.data[slideIdx]);
+    fetch('https://www.affirmations.dev')
+      .then(response => {
+        // Check if the response was successful (e.g., status code 200-299)
+        if (!response.ok) {
+          throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        // Parse the response body as JSON
+        setSlide(response.json());
+      })
+      .then(data => {
+        console.log(data); // Process the fetched data
+      })
+      .catch(error => {
+        console.error('Error fetching data:', error); // Handle any errors
+      });
+    
+    
 }
 
 const setSlide = (slideData) => {
-    document.getElementById("slideTitle").innerHTML = slideData.title;
+    document.getElementById("slideTitle").innerHTML = "Daily Affirmation";
     let slideText = document.getElementById("slideText");
-    slideText.innerHTML = slideData.text;
-    slideText.classList = "";
-    slideText.classList.add(slideData.size);
+    slideText.innerHTML = slideData.affirmation;
 }
 
 document.getElementById("nextSlide").onclick = nextSlide;
 
-setSlide(SLIDES.data[0]);
+nextSlide();
