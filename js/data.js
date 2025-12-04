b630d58a142374b76fc8cac1f3295d77983a7cac
Sam <smauer@users.noreply.github.com>
2025-12-04 10:41:10 -0600

    Update data.js again

--- js/data.js
+++ js/data.js
@@ -1,40 +1,13 @@
-var SLIDES = {
-    "data": [
-        {
-            "title": "Slide 1",
-            "text": "Can I get this working?",
-            "size": "lrg"
-        },
-        {
-            "title": "Slide 2",
-            "text": "And then this one?",
-            "size": "std"
-        },
-        {
-            "title": "Slide 3",
-            "text": "Third slide!",
-            "size": "lrg"
-        },
-        {
-            "title": "New Slide",
-            "text": "New Text",
-            "size": "lrg"
-        }
-    ]
-}
-var slideIdx = -1;
 const nextSlide = function() {
-    //if (++slideIdx > SLIDES.data.length-1) {
-    //    slideIdx = 0;
-    //}
-    //console.log(SLIDES.data[slideIdx]);
-    //setSlide(SLIDES.data[slideIdx]);
-    fetch(`https://api.allorigins.win/get?url=${encodeURIComponent('https://www.affirmations.dev')}`)
+    fetch(`https://api.allorigins.win/get?url=${encodeURIComponent('https://affirmations.dev')}`)
         .then(response => {
         	if (response.ok) return response.json()
         	throw new Error('Network response was not ok.')
         })
-        .then(data => setSlide(data.contents));
+        .then(data => {
+            console.log(data.contents);
+            setSlide(data.contents);
+        });
 }
 
 const setSlide = (slideData) => {
